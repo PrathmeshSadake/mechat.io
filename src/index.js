09c0473ced5b94a504ec560a899c98de2a13da1e
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-08-19 19:06:49 +0530

    basic socket.io connection between client and server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.json({ message: "Hello" });
+const server = http.createServer(app);
+const io = socketio(server);
+const publicDirectoryPath = path.join(__dirname, "..", "public");
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", () => {
+  console.log("New web socket connection");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
